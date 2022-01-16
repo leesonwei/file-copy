2df53940698259537a84ba4e0595701a432a498b
lizong <weilizong2014@hotmail.com>
2022-01-16 18:14:46 +0800

    ignore added

---
+++ src/main/java/com/deltaww/filecopy/FileCopyApplication.java
@@ -0,0 +1,13 @@
+package com.deltaww.filecopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCopyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCopyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deltaww/filecopy/FileCopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deltaww.filecopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCopyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
